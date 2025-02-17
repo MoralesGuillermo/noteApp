c88cb3e33c90935221106c82898be2e40dd37d95
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-02-16 23:55:20 -0600

    Implemented home view

--- views/index.view.php
+++ views/index.view.php
@@ -3,10 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../styles.css">
+    <title>Smart Pen</title>
 </head>
 <body>
-    <h1>This is the home page of my page!</h1>
-    
+    <nav class="flex justify-between align-center bg-dark text-primary py-1">
+        <div class="px-1 flex align-center">
+            <img src="images/noteApp_logo.png" alt="Logo" height="48" width="48" class="float-left">
+            <span class="mx-1 logo-text">Smart Pen</span>
+        </div>
+        <div>
+            <ul class="no-style">
+                <li class="inline"><a class="text-primary" href="">Home</a></li>
+                <li class="inline"><a class="text-primary" href="">Notes</a></li>
+                <li class="inline"><a class="text-primary" href="">About us</a></li>
+            </ul>
+        </div>
+        <div class="px-1">
+            <span>Notifications</span>
+            <span>User</span>
+        </div>
+    </nav>
+    <header>
+        <div class="header py-2 px-2">
+            <h1 class="text-secondary p1">Home</h1>
+        </div>
+    </header>
+    <main>
+        <div class="p1 my-2">
+            <!--A call to action button. To show your notes -->
+            <a class="py-2 px-2 my-1" href="">Check my notes</a>
+        </div>
+    </main>
 </body>
 </html>
