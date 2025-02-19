5b2ed80332d0140c2c9c8a576512a10b4cd68f45
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-02-18 23:55:09 -0600

    Change 404 and 405 pages' style

--- views/405.view.php
+++ views/405.view.php
@@ -5,9 +5,9 @@
     <!--Import the navigation bar-->
     <?php require_once basePath("views/partials/nav.php")?>
     <!--Import the header section of the page-->
-    <?php require_once basePath("views/partials/header.php")?>
     <main>
-        <div class="p1 my-2">
+        <div class="p1">
+            <h1 class="my-2 px-2">Method not Supported</h1>
             <!--A call to action button. To show your notes -->
             <a class="py-2 px-2 my-1" href="/">Method not Supported. Go back to home page...</a>
         </div>
